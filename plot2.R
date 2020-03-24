bafda2929c76dcadee6dab8e599482e87188d8f0
parviagrawal <62506641+parviagrawal@users.noreply.github.com>
2020-03-24 14:04:06 +0530

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,29 @@
+require(dplyr)
+
+data_file <- tempfile()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
+              destfile = data_file)
+
+data_file <- unzip(zipfile = data_file)
+
+data <- read.table(data_file, header = TRUE, sep = ";", stringsAsFactors = FALSE)
+
+rm(data_file)
+
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+data <- filter(data, Date >= "2007-02-01" & Date <= "2007-02-02")
+
+data$Time <- as.POSIXct(strptime(
+        paste(data$Date, data$Time),
+        format = "%Y-%m-%d %H:%M:%S"))
+
+png("plot2.png", width=480, height=480)
+
+with(data,
+     plot(Global_active_power~Time,
+          ylab = "Global Active Power (kilowatts)",
+          xlab = "",
+          type = "l"))
+
+dev.off()
